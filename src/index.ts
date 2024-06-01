af21b59960fa593ef8a7862f3e4acae06731776a
PenaPC <luis_alejandro241096@hotmail.com>
2024-05-31 18:20:14 -0600

    Last endoints

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import express, { Request, Response } from 'express';
+import express, { Request, Response, NextFunction } from 'express';
 import nodemailer from 'nodemailer';
 import admin from 'firebase-admin';
 import path from 'path';
@@ -29,15 +29,55 @@ const corsOptions ={
 app.use(cors(corsOptions));
 // Middleware to parse JSON bodies
 app.use(express.json());
-
-
 const unlinkAsync = promisify(fs.unlink);
+
+export function authenticateToken(req: Request, res: Response, next: NextFunction) {
+    const authHeader = req.headers['authorization'];
+    const token = authHeader && authHeader.split(' ')[1];
+
+    if (token == null) {
+        return res.status(401).json({ message: 'Token is missing' });
+    }
+
+    if (token !== process.env.SECURITY_TOKEN) {
+        return res.status(403).json({ message: 'Invalid token' });
+    }
+
+    next();
+}
+
 async function cleanUploadsDir(directory: string) {
     const files = await promisify(fs.readdir)(directory);
     const unlinkPromises = files.map(filename => unlinkAsync(path.join(directory, filename)));
     await Promise.all(unlinkPromises);
 }
+async function addEmailToNewsletter(name: string, email: string) {
+    try {
+        // Query to find the document with the specified name
+        const querySnapshot = await firestore.collection('newsletters').where('name', '==', name).get();
 
+        if (querySnapshot.empty) {
+            throw new Error(`No newsletter found with the name: ${name}`);
+        }
+
+        const newsletterDoc = querySnapshot.docs[0];
+        const newsletterData = newsletterDoc.data();
+        if (newsletterData.recipientList.includes(email)) {
+            throw new Error(`Email ${email} is already in the recipient list.`);
+        }
+
+        // Add the email to the recipient list
+        const updatedRecipientList = [...newsletterData.recipientList, email];
+
+        // Update the document in Firestore
+        await newsletterDoc.ref.update({ recipientList: updatedRecipientList });
+
+        return { message: `Email ${email} successfully added to the newsletter ${name}.` };
+    } catch (error) {
+        console.error('Error adding email to newsletter:', error);
+        return {error: error}
+    }
+}
 // Function to remove an email from the newsletter's recipient list
 async function removeEmailFromNewsletter(name: string, email: string) {
     try {
@@ -48,20 +88,18 @@ async function removeEmailFromNewsletter(name: string, email: string) {
         throw new Error(`No newsletter found with the name: ${name}`);
         }
 
-        // Assuming names are unique and only one document will be found
         const newsletterDoc = querySnapshot.docs[0];
         const newsletterData = newsletterDoc.data();
-
-        // Check if the email exists in the recipient list
         if (!newsletterData.recipientList.includes(email)) {
             throw new Error(`Email ${email} not found in the recipient list.`);
         }
 
         // Remove the email from the recipient list
         const updatedRecipientList = newsletterData.recipientList.filter((recipientEmail: string) => recipientEmail !== email);
-
+        const currentUnsubscribeCount = newsletterData.unsubscribeCount || 0;
+        const newUnsubscribeCount = currentUnsubscribeCount + 1;
         // Update the document in Firestore
-        await newsletterDoc.ref.update({ recipientList: updatedRecipientList });
+        await newsletterDoc.ref.update({ unsubscribeCount: newUnsubscribeCount, recipientList: updatedRecipientList  });
 
         return { message: `Email ${email} successfully removed from the newsletter ${name}.` };
     } catch (error) {
@@ -74,14 +112,16 @@ async function removeEmailFromNewsletter(name: string, email: string) {
 async function getNewslettersWithRecipientCount() {
     try {
         const querySnapshot = await firestore.collection('newsletters').get();
-        const newsletters: { name: string, recipients: number }[] = [];
+        const newsletters: { name: string, recipients: number, emailsSent: number, fileName: string, unsubscribeCount: number}[] = [];
 
         querySnapshot.forEach(doc => {
             const data = doc.data();
             const name = data.name;
             const recipients = Array.isArray(data.recipientList) ? data.recipientList.length : 0;
-
-            newsletters.push({ name, recipients });
+            const emailsSent = data.sentEmailsCount || 0
+            const fileName = data.attachmentPath
+            const unsubscribeCount = data.unsubscribeCount
+            newsletters.push({ name, recipients, emailsSent, fileName, unsubscribeCount});
         });
 
         return newsletters;
@@ -117,8 +157,8 @@ async function sendEmail(to: string, subject: string, text: string, html: string
         service: 'Gmail',
         auth: {
         user: process.env.EMAIL_USER,
-        pass: process.env.EMAIL_PASS
-        }
+        pass: process.env.EMAIL_PASS,
+        },
     });
 
     const mailOptions = {
@@ -130,23 +170,23 @@ async function sendEmail(to: string, subject: string, text: string, html: string
         attachments: [
         {
             filename: path.basename(attachmentPath),
-            path: attachmentPath
-        }
-        ]
+            path: attachmentPath,
+        },
+        ],
     };
 
     await transporter.sendMail(mailOptions);
 }
 
 // Route to handle email sending
-app.post('/send-newsletter', async (req: Request, res: Response) => {
+app.post('/send-newsletter', authenticateToken, async (req: Request, res: Response) => {
     const { name } = req.body;
     const newsletters = await getNewslettersByName(name);
     if (newsletters.length == 0) {
         res.status(200).send('Error no newsletter found with name.' + name);
     }
     const newsletter = newsletters[0]
-    console.log('Newsletters:', newsletter);
+    console.log('Newsletter to send:', newsletter);
     const emails = newsletter.recipientList
     const attachmentPath = newsletter.attachmentPath
     if (!emails || !Array.isArray(emails) || emails.length === 0) {
@@ -175,6 +215,18 @@ app.post('/send-newsletter', async (req: Request, res: Response) => {
         );
         }
         await cleanUploadsDir(path.join(__dirname, 'uploads'));
+        // Update Firestore with the count of sent emails
+        const querySnapshot = await firestore.collection('newsletters').where('name', '==', name).get();
+
+        if (querySnapshot.empty) {
+            throw new Error(`Error finding the newsletter: ${name}`);
+        }
+        const newsletterDoc = querySnapshot.docs[0];
+        const newsletterData = newsletterDoc.data();
+        const currentSentEmailsCount = newsletterData.sentEmailsCount || 0;
+        const newSentEmailsCount = currentSentEmailsCount + emails.length;
+
+        await newsletterDoc.ref.update({ sentEmailsCount: newSentEmailsCount });
         res.status(200).send('Newsletter sent successfully!');
     } catch (error) {
         console.error('Error sending email:', error);
@@ -183,7 +235,7 @@ app.post('/send-newsletter', async (req: Request, res: Response) => {
 });
 
 // endpoint to register the newsletter in Firestore
-app.post('/register-newsletter', async (req: Request, res: Response) => {
+app.post('/register-newsletter', authenticateToken, async (req: Request, res: Response) => {
     const name  = req.body.name;
     const recipientList = JSON.parse(req.body.recipientList);
     const attachmentPath = req.body.attachmentPath;
@@ -213,7 +265,7 @@ app.post('/register-newsletter', async (req: Request, res: Response) => {
 });
 
 // Endpoint to fetch newsletters
-app.get('/newsletters', async (req: Request, res: Response) => {
+app.get('/newsletters', authenticateToken, async (req: Request, res: Response) => {
     try {
         const newsletters = await getNewslettersWithRecipientCount();
         res.status(200).json(newsletters);
@@ -222,12 +274,27 @@ app.get('/newsletters', async (req: Request, res: Response) => {
     }
 });
 
+// Endpoint to add an email to the newsletter
+app.post('/add-email', authenticateToken, async (req, res) => {
+    const { name, email } = req.body;
+
+    if (!name || !email) {
+        return res.status(400).send('Name and email are required.');
+    }
+
+    try {
+        const result = await addEmailToNewsletter(name, email);
+        res.status(200).send({success: result.message});
+    } catch (error) {
+        res.status(500).send('Error adding email to the newsletter.');
+    }
+});
+
+
 // endpoint to unsubscribe
 app.get('/unsubscribe', (req: Request, res: Response) => {
     const email = req.query.email as string;
     const name = req.query.name as string;
-
-    console.log(req.query);
     
     if (email && name) {
     // Logic to handle unsubscription
